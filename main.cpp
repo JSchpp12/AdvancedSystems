a7aa677239b4df84299fef9e16582d670b556ad7
jacob <jacob.schaupp@rockets.utoledo.edu>
2020-02-10 15:27:00 -0500

    First Test

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+int main() {
+    //phase 1 requires that numerous pieces of information be collected and stored for later use
+    string username, password;
+    cout << "Hello, World!" << std::endl;
+    return 0;
+}
